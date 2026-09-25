2cc0c05d56af22d7c9a79d1a547d6dba6eb8edf0
komal Aulakh <kkomalpreetkaur3@academic.rrc.ca>
2026-09-25 11:37:02 -0500

    Add ingredient IDs to recipes

--- src/components/recipelist/recipelist.tsx
+++ src/components/recipelist/recipelist.tsx
@@ -4,16 +4,19 @@ function RecipeList() {
       id: 1,
       name: "Creamy Alfredo Pasta",
       description: "Creamy pasta made with cheese, garlic, and a rich sauce.",
+      ingredients: [1, 2, 3, 4, 5],
     },
     {
       id: 2,
       name: "Chicken Tacos",
       description: "Soft tacos filled with seasoned chicken and fresh vegetables.",
+      ingredients: [2, 6, 7, 8, 9],
     },
     {
       id: 3,
       name: "Chocolate Cake",
       description: "Soft chocolate cake with a rich and sweet chocolate flavour.",
+      ingredients: [4, 10, 11, 12, 13, 14],
     },
   ];
 
