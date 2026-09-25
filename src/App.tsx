c3990c8b4c1156f22e1e6ed47f63353f8e9dfc1e
komal Aulakh <kkomalpreetkaur3@academic.rrc.ca>
2026-09-25 09:38:37 -0500

    Add multi-page navigation with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,29 @@ import './App.css'
 import RecipeList from './components/recipelist/recipelist'
 import IngredientsList from './components/ingredients/ingredients'
 import Reviews from './components/reviews/reviews'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <header>
         <h1>FoodieHub</h1>
         <p>Discover recipes, ingredients, and reviews</p>
       </header>
 
       <main>
-        <RecipeList />
-        <IngredientsList />
-        <Reviews />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipes" element={<RecipeList />} />
+          <Route path="/ingredients" element={<IngredientsList />} />
+          <Route path="/reviews" element={<Reviews />} />
+        </Routes>
       </main>
 
       <footer>
         <p>Created by Komal, Amrinder, and Muskan</p>
       </footer>
-    </>
+    </BrowserRouter>
   )
 }
 
